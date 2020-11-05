553d5d2c498cc9234d8f9cd44882c2cb60d87086
225550 <60364282+225550@users.noreply.github.com>
2020-11-05 14:26:39 -0600

    nonstupidity
    fixing and nonstupidity

--- Liampd.py
+++ Liampd.py
@@ -6,9 +6,9 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'liam murray' # Only 10 chars displayed.
+strategy_name = 'fucher black mail'
+strategy_description = 'if you step out of line I will all ways betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -25,12 +25,12 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
+    if 'b' in their_history or len(their_history) == 199:
+        return 'b'
     for i in their_history:
         if not( i == 'b' or i == 'c'):
             return 'b'
-    if 'b' in their_history or len(their_history) == 199:
-        return 'b'
-    else: return 'c'
+    return 'c'
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
