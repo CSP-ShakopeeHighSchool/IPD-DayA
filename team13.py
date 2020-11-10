0808e3462b5f32cfc8aa04d9fd7df95cd0cfa714
Kael Lunser <228314@shakopeeschools.org>
2020-11-10 13:53:38 -0600

    Updated team score

--- team13.py
+++ team13.py
@@ -6,9 +6,10 @@
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = 'Kael' # Only 10 chars displayed.
+strategy_name = 'To collude or betray?'
+strategy_description = 'if they have 30%<colludes then collude, if they have 30%<betray then betray'
+import random    
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -17,7 +18,17 @@ def move(my_history, their_history, my_score, their_score):
     Make my move.
     Returns 'c' or 'b'. 
     '''
-
+    if len(my_history) == 0: 
+        return 'c' or 'b'
+    bcount = 0
+    for i in their_history:
+        if i == 'b':
+            bcount += 1
+    if bcount > .3*len(their_history):
+            return 'b'
+    else:
+            return 'c'
+        
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
