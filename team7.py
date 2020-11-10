e33538816df4e615d99804880bf35ef6667d5929
NoahPeterson <73717263+NoahPeterson@users.noreply.github.com>
2020-11-10 14:22:35 -0600

    test

--- team7.py
+++ team7.py
@@ -8,7 +8,7 @@
 ####
 
 team_name = 'Team 7' # Only 10 chars displayed.
-strategy_name = 'Noahs Strat'
+strategy_name = 'Noahs Strat test'
 strategy_description = 'How does this strategy decide?'
     
 def move(my_history, their_history, my_score, their_score):
