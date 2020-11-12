b254b3ad288fd3d9ee947b274142c424a498c788
Xbruh <73908469+Xbruh@users.noreply.github.com>
2020-11-12 13:17:43 -0600

    Revert "Update team8.py"
    This reverts commit 7924e76452e81d6b93e5f180990719e9706e1f67.

--- team8.py
+++ team8.py
@@ -26,16 +26,7 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-   
-    if len(their_history) % 5 == 0:
-        return 'b'
-    if my_score < -2000:
-            return 'b'
-    elif my_score > -2000:
-        return 'c'
-    if their_history == 'b':
-        return 'b'
-           
+    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
