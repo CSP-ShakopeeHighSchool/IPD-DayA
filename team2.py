a94d5a67de3af39bccc01b487b0fce0710103240
WillWood03 <73908531+WillWood03@users.noreply.github.com>
2020-11-12 13:03:46 -0600

    Update team2.py

--- team2.py
+++ team2.py
@@ -7,26 +7,14 @@
 ####
 
 team_name = 'Will W'
-strategy_name = 'Collude as long as they collude, then betray/test'
-strategy_description = 'Strategy will collude to start, and collude as long as opponent colludes, once betrayed betray 25 times, then test if they colluded'
+strategy_name = 'Collude if even, if not betray'
+strategy_description = 'Strategy will collude to start, then betray if the scores are different'
 
 def move(my_history, their_history, my_score, their_score):
-    if len(my_history)==0: 
-        return 'c' 
-    if 'b' in their_history:
-        count = 0
-        for i in their_history:
-            if 'b' == i:
-                count +=1
-            if count >25 and 'c' in their_history[-5:]:
-                return 'c'
-            else:
-                return 'b'
-    else:
+    if my_score == their_score:
         return 'c'
-                
-                    
-
+    else:
+        return 'b'
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -35,7 +23,6 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
